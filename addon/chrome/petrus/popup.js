87a9f527a961d76fdbdd961bf85fc82f4f3676b8
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2019-08-09 09:17:01 +0200

    [wip] added chrome extension and trend view prototypes

---
+++ addon/chrome/petrus/popup.js
@@ -0,0 +1,37 @@
+function onPageDetailsReceived(pageDetails) {
+    document.getElementById('jira_key').value = pageDetails.jira_key;
+}
+
+var statusDisplay = null;
+
+function estimate() {
+
+    event.preventDefault();
+
+    var jira_key = document.getElementById('jira_key');
+    var getUrl = 'http://localhost:55777/?jira_key=' + encodeURIComponent(jira_key.value);
+    var formContentType = 'application/x-www-form-urlencoded';
+    try {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', getUrl, true);
+        xhr.setRequestHeader('Content-type', formContentType);
+        xhr.onreadystatechange = function() {
+            if(xhr.responseText)
+                statusDisplay.innerHTML = xhr.responseText;
+            else
+                statusDisplay.innerHTML = 'Done.';
+        };
+        xhr.send();
+    } catch(e) {
+        statusDisplay.innerHTML = e.message;
+    }
+    statusDisplay.innerHTML = 'Saving...';
+}
+
+window.addEventListener('load', function(evt) {
+    statusDisplay = document.getElementById('status-display');
+    document.getElementById('update').addEventListener('submit', estimate);
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        eventPage.getPageDetails(onPageDetailsReceived);
+    });
+});
---
+++ www/sd.js
@@ -0,0 +1,144 @@
+var sd;
+
+SD = (function() {
+
+    var self;
+
+    var construct = function() {
+        self = this;
+        self.requestTrend();
+    };
+
+    function refresh(response) {
+        self.rotatePointer("tickets-per-hour", response["tickets-per-hour"], 4);
+        self.rotatePointer("hours-total", response["hours-total"], 240);
+        self.onTheRoad(response["hot-projects"]);
+        self.updateTicketsTracked(response["tickets-tracked"]);
+        self.updateLabel("tickets-per-hour", response["tickets-per-hour"]);
+        self.updateLabel("hours-total", response["hours-total"]);
+        self.updateBars(response["payed-seconds"], response["bb5-seconds"], response["un-payed-seconds"]);
+    }
+
+    function rotatePointer(key, value, max) {
+        var degree = value / max * 240 - 240 / 2;
+        $('[data-js="' + key + '"] .pointer').animate({deg: degree}, {step: function(now) {$('[data-js="' + key + '"] .pointer').css({"transform": "rotate(" + now + "deg)"});}, duration: 500});
+    }
+
+    function getRandomNumber(min, max, decimals) {
+        return (Math.random() * (min - max) + max).toFixed(decimals);
+    }
+
+    function onTheRoad(projects) {
+        $('.cyan-light').show();
+        $('.red-light').hide();
+        $('.blue-light').hide();
+        $('.dark').css({'opacity': 0.5});
+
+        for(var i in projects) {
+            $('.road-' + i).text(projects[i][0] + " (" + self.reduceToTwoDecimals(projects[i][1]) + " h)");
+            if(parseInt(projects[i][1]) >= 20) {
+                $('.road-' + i).css({'color': 'Crimson'});
+                $('.cyan-light').hide();
+                $('.red-light').show();
+                $('.blue-light').show();
+                $('.dark').css({'opacity': 1});
+            } else if(parseInt(projects[i][1]) >= 10) {
+                $('.road-' + i).css({'color': 'SandyBrown'});
+            } else if(parseInt(projects[i][1]) >= 5) {
+                $('.road-' + i).css({'color': 'Moccasin'});
+            } else {
+                $('.road-' + i).css({'color': 'Cornsilk'});
+            }
+        }
+
+    }
+
+    function reduceToTwoDecimals(value) {
+
+        return (parseInt(value * 100) / 100)
+
+    }
+
+    function requestTrend() {
+
+        $.get( "http://localhost:55666/", function(response) {
+          console.log(response);
+        });
+        $.get( "trend.json", function(trend) {
+          self.refresh(JSON.parse(trend));
+        });
+
+    }
+
+    function updateTicketsTracked(ticketsTracked) {
+
+        $('[data-js="tickets-tracked"] .number').text(ticketsTracked);
+
+    }
+
+    function updateLabel(key, value) {
+
+        $('[data-js="' + key + '"] .label span').text(self.reduceToTwoDecimals(value));
+
+    }
+
+    function updateBars(support, maintenance, bugfix) {
+
+        var sum = support + maintenance + bugfix;
+        var supportPercent = self.reduceToTwoDecimals(support / sum * 100);
+        var maintenancePercent = self.reduceToTwoDecimals(maintenance / sum * 100);
+        var bugfixPercent = self.reduceToTwoDecimals(bugfix / sum * 100);
+        self.drawBar('support', supportPercent);
+        self.drawBar('maintenance', maintenancePercent);
+        self.drawBar('bugfix', bugfixPercent);
+
+    }
+
+    function drawBar(bartype, percentage) {
+        var integer_percentage = parseInt(percentage);
+        var decimals = parseInt((percentage-integer_percentage)*100);
+        $('.bar.'+bartype).css("height", percentage + "%");
+        $('.bar.'+bartype+' span').text(integer_percentage + "%");
+        if(decimals) {
+            $('.bar.'+bartype+' span').append("<br /><i>." + decimals + "</i>")
+        }
+        if(percentage < 50) {
+            $('.bar.'+bartype).css("box-shadow", "none");
+        }
+    }
+
+    construct.prototype = {
+        refresh: refresh,
+        rotatePointer: rotatePointer,
+        getRandomNumber: getRandomNumber,
+        onTheRoad: onTheRoad,
+        requestTrend: requestTrend,
+        updateTicketsTracked: updateTicketsTracked,
+        updateLabel: updateLabel,
+        reduceToTwoDecimals: reduceToTwoDecimals,
+        updateBars: updateBars,
+        drawBar: drawBar
+    };
+
+    return construct;
+
+})();
+
+sd = new SD();
+
+function reloadMe() {
+    window.location.reload();
+}
+function progressGo() {
+    var $prog = $('progress');
+    var value = $prog.val();
+    var max = $prog.attr('max');
+    if(value < max) {
+        value += 1;
+    }
+    $prog.val(value);
+    if(value >= max) {
+        setTimeout(reloadMe, 1000);
+    }
+}
+setInterval(progressGo, 100);
