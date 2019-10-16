7c4e9a37fc74e0d97c6f0fac5f3dbc3ca1f239bf
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2019-10-16 12:03:49 +0200

    issues/9 - implemented chrome extension search

--- addon/chrome/petrus/popup.js
+++ addon/chrome/petrus/popup.js
@@ -25,6 +25,39 @@ function estimate() {
     statusDisplay.innerHTML = 'Saving...';
 }
 
+function search() {
+
+    event.preventDefault();
+
+    document.getElementById('link-list').innerHTML = '';
+    var query = document.getElementById('query');
+    var getUrl = 'http://192.168.6.152:55888/?function=Search&keywords=' + encodeURIComponent(query.value);
+    var formContentType = 'application/x-www-form-urlencoded';
+    try {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', getUrl, true);
+        xhr.setRequestHeader('Content-type', formContentType);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === xhr.DONE) {
+                if (xhr.status === 200) {
+                    var json_parsed = JSON.parse(xhr.responseText);
+                    if(json_parsed && typeof json_parsed.items !== 'undefined') {
+                        var parsed_list = json_parsed.items[0].relevancy;
+                        document.getElementById('link-list').innerHTML = '';
+                        for(var i in parsed_list) {
+                            document.getElementById('link-list').innerHTML += '<p><a href="' + parsed_list[i].link + '" target="_blank">' + parsed_list[i].title + ' [' + parseInt(parsed_list[i].percentage) + '%]</a></p>';
+                        }
+                    }
+                }
+            }
+        };
+        xhr.send();
+    } catch(e) {
+        console.log(e.message);
+    }
+
+}
+
 function ping() {
     document.getElementById('is-online').innerHTML = 'pending';
     var getUrl = 'http://192.168.6.152:55888/?function=Ping';
@@ -50,6 +83,7 @@ window.addEventListener('load', function(evt) {
     ping();
     statusDisplay = document.getElementById('status-display');
     document.getElementById('update').addEventListener('submit', estimate);
+    document.getElementById('search').addEventListener('submit', search);
     chrome.runtime.getBackgroundPage(function(eventPage) {
         eventPage.getPageDetails(onPageDetailsReceived);
     });
