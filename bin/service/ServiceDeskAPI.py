94ffd5779bb29d43dda0114e8218ce4c762dd71b
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2019-12-16 12:28:43 +0100

    Added comment request

--- bin/service/ServiceDeskAPI.py
+++ bin/service/ServiceDeskAPI.py
@@ -126,10 +126,11 @@ class ServiceDeskAPI:
         comment_url = comment_endpoint.format(mapped_ticket['ID'])
         response, content = self.client.request(comment_url, "GET")
         if response['status'] != '200':
-            mapped_ticket['Worklog'] = None
+            mapped_ticket['Comments'] = None
             return mapped_ticket
         comments_raw = content.decode("utf-8")
         mapped_ticket['Comments'] = json.loads(comments_raw)
+        return mapped_ticket
 
     def request_info(self):
         info_endpoint = self.environment.get_endpoint_info()
