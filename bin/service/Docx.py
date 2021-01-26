12e9a2db68637b330512316914a2727c63846b09
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2021-01-26 14:19:20 +0100

    wording

--- bin/service/Docx.py
+++ bin/service/Docx.py
@@ -312,9 +312,9 @@ class Docx:
             elif axis == "closed tickets/day":
                 headline = "Geschlossene Tickets am Tag"
             elif axis == "average days/priority":
-                headline = "Durchschnittliche Dauer zwischen Öffnung und Schließung"
+                headline = "Durchschnittliche Lebensdauer pro Priorität"
             self.document.add_heading(headline, level=1)
-            self.document.add_picture(sub_plot_path, width=Inches(4))
+            self.document.add_picture(sub_plot_path, width=Inches(5))
 
             i += 1
 
