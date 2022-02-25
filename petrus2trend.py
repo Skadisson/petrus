24b85225a7f74aea3c7ab930d3e4b22c40a2e33c
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2022-02-25 10:30:30 +0100

    time span fix

--- petrus2trend.py
+++ petrus2trend.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     from datetime import date
     import time
     year = date.fromtimestamp(time.time()).strftime("%Y")
-    trend = Trend.Trend(months=1, year=year, week_numbers="", start=-1)
+    trend = Trend.Trend(months=1, year=year, week_numbers="", start=0)
     items, success = trend.run()
     print(success)
 
